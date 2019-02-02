5794b09e2c1a32b737d14d3856bfb2cdcdd09d00
Unknown <16adm7@queensu.ca>
2019-02-02 03:58:02 -0500

    made tutorial page

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
-export default class App extends React.Component {
+const styles = StyleSheet.create({
+  mainFont: {
+    color: 'red',
+    fontSize: 25,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    lineHeight: 130,
+  },
+  imageStyle: {
+    height: 400,
+  },
+  containerStyle: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
+
+export default class difFonts extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <View style={styles.containerStyle}>
+        <Image
+          source={require('./assets/rideyeetlogo.png')} style={styles.imageStyle} />
+        <Text style={styles.mainFont}>WELCOME TO RIDEYEET</Text>
+      </View >
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
