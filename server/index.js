ed21033bb4482dc3f928ff14ea8d72fb76743672
gabbybermudez1 <gabbybermudez1@gmail.com>
2019-02-02 16:23:14 -0500

    added some middleware and planning the first route
    first route is /trips

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@
 const express = require('express');
 const ip = require('ip');
 const axios = require('axios');
-
+const bodyParser = ('body-parser');
 
 // ===========================
 // LOCAL IMPORTS
@@ -14,13 +14,19 @@ const { kijijiScraper } = require('./kijiji');
 
 
 // ===========================
-// global variable declarations
+// global variable declarations and middleware
 // ===========================
 const port = process.env.port || 8080;
 const app = express();
+app.use(bodyParser.json());
 
 
+// app.get('/trips', (req, res) => {
 
+// })
+
+
+// app listens on available port
 app.listen(port, () => {
     console.log('server started at http://' + ip.address() + ':' + port)
 });
