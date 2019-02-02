7227850f3e67cded3c1fb64f7937fb635716082c
Chris Givelas <chris_g_50@hotmail.com>
2019-02-02 15:07:18 -0500

    Partial commit: For development purposes, render tutorial page by default

--- App.js
+++ App.js
@@ -1,36 +1,9 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
-import { Tutorial } from 'tutorial.js';
-
-const styles = StyleSheet.create({
-  mainFont: {
-    color: 'red',
-  },
-  fontSize: 25,
-  fontWeight: 'bold',
-  textAlign: 'center',
-  lineHeight: 130,
-  imageStyle: {
-    height: 400,
-  },
-  containerStyle: {
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
+import React from "react"
+import { StyleSheet, Text, View, Image, Button } from "react-native"
+import Tutorial from "./tutorial"
 
 export default class difFonts extends React.Component {
-  render() {
-    return (
-      <Tutorial />
-      // <View style={styles.containerStyle}>
-      //   <Image
-      //     source={require('./assets/rideyeetlogo.png')} style={styles.imageStyle} />
-      //   <Text style={styles.mainFont}>WELCOME TO RIDEYEET</Text>
-      //   <Button title='proceed' color='red'><text> Proceed</text></Button>
-      //   <Button title='skip' color='black'><text>Skip</text></Button>
-      // </View >
-    );
-  }
+    render() {
+        return <Tutorial />
+    }
 }
-
--- tutorial.js
+++ tutorial.js
@@ -1,87 +1,79 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
-
-export class Tutorial extends React.Component {
-
-    render() {
-        return (
-            <View style={styles.container}>
-                <Image
-                    style={styles.gologo}
-                    source={"./assets/goLogo.jpg"}
-                />
-                <Image
-                    style={styles.megalogo}
-                    source={"./assets/megaBus.jpg"}
-                />
-                <Image
-                    style={styles.kajlogo}
-                    source={"./assets/kajLogo.png"}
-                />
-                <Image
-                    style={styles.vialogo}
-                    source={"./assets/viaLogo.jpg"}
-                />
-                <Text style={style.aboutheader}>What we do</Text>
-                <Text style={style.aboutbody}>Lorem ipsum dolor sit amet, ei saperet eleifend percipitur sed. Pri idque ignota ad. Ut sea diam aeque cetero, vis porro voluptua menandri no, liber bonorum nam ei. Pro causae constituam inciderint te, denique nominavi suavitate cu mel.</Text>
-                <Button title="back"><Text>back</Text></Button>
-            </View>
-        );
-    }
-}
+import React from "react"
+import { StyleSheet, Text, View, Button, Image } from "react-native"
 
 const styles = StyleSheet.create({
     container: {
-        display: flex,
+        display: "flex",
         flex: 1,
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'Seashell',
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#fff5ee"
     },
     gologo: {
         width: 100,
         height: 100,
         borderRadius: 25,
-        margin: 'auto',
-
+        margin: "auto"
     },
     megalogo: {
         width: 100,
         height: 100,
-        borderRadius: "25px",
-        margin: 'auto',
+        borderRadius: 25,
+        margin: "auto"
     },
     kajlogo: {
         width: 100,
         height: 100,
-        borderRadius: "25px",
-        margin: 'auto',
+        borderRadius: 25,
+        margin: "auto"
     },
     vialogo: {
         width: 100,
         height: 100,
-        borderRadius: "25px",
-        margin: 'auto',
+        borderRadius: 25,
+        margin: "auto"
     },
     aboutheader: {
-        font: "verdana",
-        fontWeight: 'bold',
-        fontSize: '24em',
-        textColor: 'FF5C5C',
-        margin: 'auto',
+        // fontFamily: "Verdana",
+        fontWeight: "bold",
+        fontSize: 24,
+        color: "#FF5C5C",
+        margin: "auto"
     },
     aboutbody: {
-        font: 'verdana',
-        fontSize: '12em',
-        textColor: 'DarkSlateGrey',
-        margin: 'auto',
+        // fontFamily: "Verdana",
+        fontSize: 12,
+        color: "#2f4f4f",
+        margin: "auto"
     },
     backbutton: {
-        font: 'verdana',
-        fontSize: '12em',
-        color: 'DarkSlateGrey',
-        margin: 'auto',
+        // fontFamily: "Verdana",
+        fontSize: 12,
+        color: "#2f4f4f",
+        margin: "auto"
     }
-});
+})
+
+export default class Tutorial extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Image style={styles.gologo} source={"./assets/goLogo.jpg"} />
+                <Image style={styles.megalogo} source={"./assets/megaBus.jpg"} />
+                <Image style={styles.kajlogo} source={"./assets/kajLogo.png"} />
+                <Image style={styles.vialogo} source={"./assets/viaLogo.jpg"} />
+                <Text style={styles.aboutheader}>What we do</Text>
+                <Text style={styles.aboutbody}>
+                    Lorem ipsum dolor sit amet, ei saperet eleifend percipitur sed. Pri idque ignota ad. Ut sea diam
+                    aeque cetero, vis porro voluptua menandri no, liber bonorum nam ei. Pro causae constituam inciderint
+                    te, denique nominavi suavitate cu mel.
+                </Text>
+                <Button title="back">
+                    <Text>back</Text>
+                </Button>
+            </View>
+        )
+    }
+}
