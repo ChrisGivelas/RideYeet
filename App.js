0128e41fb771e463382c112bb94c41b351053401
Gabby <gabbybermudez1@gmail.com>
2019-02-02 22:39:43 -0500

    Formatted start Screen in percentages for layout

--- App.js
+++ App.js
@@ -1,41 +1,60 @@
 import React from "react"
-import { StyleSheet, Text, View, Image, Button } from "react-native"
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native"
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import Tutorial from "./tutorial";
 import BookingScreen from './screens/BookingScreen';
 
 const styles = StyleSheet.create({
-  mainFont: {
-    color: '#ff5c5c',
-    fontSize: 25,
-    // fontWeight: 'bold',
-    textAlign: 'center',
-    lineHeight: 300,
-  },
-  imageStyle: {
-    height: 300,
-    width: 390,
-  },
   containerStyle: {
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  mainFont: {
+    color: '#ff5c5c',
+    fontSize: 25,
+    textAlign: 'center',
+  },
+
+  imageStyle: {
+    height: 200,
+    width: "100%",
+    margin: 50
+  },
+
+  textContainer: {
+    height: "20%",
+    width: "100%",
+
+
+  },
+  buttonContainer: {
+    height: "20%",
+
+  },
+  buttonStyle: {
+    padding: 15,
+    backgroundColor: 'green',
+    borderRadius: 4
   }
+
 })
 
 export class DifFonts extends React.Component {
   render() {
     return (
-      <View
-        style={styles.containerStyle}>
+      <View style={styles.containerStyle}>
+
         <Image
           style={styles.imageStyle} source={require('./assets/cardrive.gif')} />
-        <Text style={styles.mainFont}>You're Ready To Start Booking!</Text>
-        <Button
-          title='Okay, Got It'
-          onPress={() => this.props.navigation.navigate('Welcome')}
-          color='green'>
-          type='clear'
-        </Button>
+
+        <View style={styles.textContainer}>
+          <Text style={styles.mainFont}>You're Ready To Start Booking!</Text>
+        </View>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity onPress={this.onPressBook} style={styles.buttonStyle} >
+            <Text style={{ color: 'white', fontWeight: "500" }}> BOOK NOW </Text>
+          </TouchableOpacity>
+        </View>
       </View >
     );
   }
--- screens/HomePage.js
+++ screens/HomePage.js
@@ -5,36 +5,30 @@ const styles = StyleSheet.create({
     mainFont: {
         color: '#ff5c5c',
         fontSize: 25,
-        // fontWeight: 'bold',
         textAlign: 'center',
-        // lineHeight: 150,
     },
     imageStyle: {
-        height: 400,
+        height: 200,
 
     },
-    containerStyle: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
+    // containerStyle: {
+    //     alignItems: 'center',
+    //     justifyContent: 'center'
+    // },
     buttonsContainer: {
-        flex: 2,
-        flexDirection: 'column',
-        justifyContent: 'space-evenly'
+        width: "100%",
+        height: "30%"
     },
     otherContainer: {
-        flex: 7
+        width: "100%",
+        height: "50%"
     },
-    padder: {
-        flex: 1
-    }
 })
 
 export default class difFonts extends React.Component {
     render() {
         return (
-            <View style={styles.containerStyle}>
+            <View >
                 <View style={styles.otherContainer}>
                     <Image
                         source={require('./assets/rideyeetlogo.png')} style={styles.imageStyle} />
