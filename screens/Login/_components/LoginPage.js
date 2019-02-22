3efbf1865c27dfc306d969090708e5abd0ee2a0d
Daniel Mousley <danieltmousley@gmail.com>
2019-02-22 18:16:35 -0500

    Started creating login page

---
+++ screens/Login/_components/LoginPage.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react"
+import { StyleSheet, Text, View, Image, TouchableOpacity, TextInput } from "react-native"
+
+const styles = StyleSheet.create({
+    page: {},
+    button: {}
+})
+
+export default class LoginPage extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            email: "sampletext",
+            password: "sampletext"
+        }
+    }
+
+    _onPressButton = () => {
+        this.props.navigation.navigate("Booking")
+    }
+
+    updateEmail = text => {
+        this.setState({ email: text })
+    }
+
+    updatePassword = text => {
+        this.setState({ password: text })
+    }
+
+    render() {
+        return (
+            <View style={styles.page}>
+                <TextInput
+                    style={styles.inputField}
+                    onChangeText
+                    {...this.updateEmail(text)}
+                    value={this.state.email}
+                />
+                <TextInput
+                    style={styles.inputField}
+                    onChangeText
+                    {...this.updatePassword(text)}
+                    value={this.state.password}
+                />
+                <TouchableOpacity onPress={this._onPressButton}>
+                    <Image style={styles.button} />
+                </TouchableOpacity>
+            </View>
+        )
+    }
+}
